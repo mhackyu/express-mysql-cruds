acacf9416604ea53608589a97fd395da81cee911
mhackyu <markpaderes0932@yahoo.com>
2018-06-12 18:52:17 +0800

    add dotenv

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mhackyuka',
-  database: 'store'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect(function (err) { 
